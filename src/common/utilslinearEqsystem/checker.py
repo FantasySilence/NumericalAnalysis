b30a22d96ea3549c72ae1b92da844f074e46868a
FantasySilence <1735793009@qq.com>
2024-03-07 17:00:26 +0800

    修改为装饰器类

--- src/common/utilslinearEqsystem/checker.py
+++ src/common/utilslinearEqsystem/checker.py
@@ -1,8 +1,39 @@
 import numpy as np
 
-def check_matrix(func):
 
-    def wrapper(cls, matrix:np.ndarray, *args, **kwargs):
+class isMatrixReasonale:
+
+    """
+    定义一个装饰器类，用于检查传入的矩阵是否满足条件
+    """
+
+    def __init__(self, model_name: str):
+        
+        self.model_name = model_name
+
+    
+    def __call__(self, func):
+
+        def wrapper(cls, matrix: np.ndarray, *args, **kwargs):
+            
+            if self.model_name == "GaussElimate":
+                self._checkForGauss(matrix)
+            if self.model_name == "Iteration":
+                self._checkForIteration(matrix)
+            if self.model_name == "RootSquare":
+                self._checkForSquare(matrix)
+            if self.model_name == "TriangleDecomposite":
+                self._checkForTriangle(matrix)
+            res = func(cls, matrix, *args, **kwargs)
+            return res
+        return wrapper
+    
+
+    def _checkForGauss(self, matrix: np.ndarray):
+
+        """
+        适用于高斯消去法
+        """
 
         check_matrix = matrix.copy()
         if check_matrix[:, :-1].shape[0] != check_matrix[:, :-1].shape[1]:
@@ -14,7 +45,47 @@ def check_matrix(func):
         if np.linalg.matrix_rank(check_matrix[:, :-1]) == np.linalg.matrix_rank(matrix) \
            and np.linalg.matrix_rank(check_matrix[:, :-1]) != check_matrix[:, :-1].shape[0]:
             raise ValueError('该线性方程组有多个解,暂时支持有唯一解的方程组')
-        result = func(cls, matrix, *args, **kwargs)
-        return result
+
+
+    def _checkForIteration(self, matrix: np.ndarray):
+
+        """
+        适用于迭代法
+        """
+
+        self._checkForGauss(matrix)
+
     
-    return wrapper
+    def _checkForSquare(self, matrix: np.ndarray):
+
+        """
+        适用于平方根分解法
+        """
+
+        check_matrix = matrix.copy()
+        if check_matrix[:,:-1].shape[0] != check_matrix[:,:-1].shape[1]:
+            raise ValueError('线性方程组的系数矩阵不是方阵')
+        if not np.allclose(check_matrix[:,:-1], check_matrix[:,:-1].T):
+            raise ValueError('线性方程组的系数矩阵不是对称矩阵')
+        if np.linalg.matrix_rank(check_matrix[:,:-1]) != np.linalg.matrix_rank(matrix):
+            raise ValueError('该线性方程组无解')
+        if np.linalg.matrix_rank(check_matrix[:,:-1]) == np.linalg.matrix_rank(matrix) \
+           and np.linalg.matrix_rank(check_matrix[:,:-1]) < check_matrix[:,:-1].shape[0]:
+            raise ValueError('该线性方程组有多个解,暂时支持有唯一解的方程组')
+    
+
+    def _checkForTriangle(self, matrix: np.ndarray):
+
+        """
+        适用于三角分解法
+        """
+
+        check_matrix = matrix.copy()
+        if check_matrix[:,:-1].shape[0] != check_matrix[:,:-1].shape[1]:
+            raise ValueError('线性方程组的系数矩阵不是方阵')
+        if np.linalg.matrix_rank(check_matrix[:,:-1]) != np.linalg.matrix_rank(matrix):
+            raise ValueError('该线性方程组无解')
+        if np.linalg.matrix_rank(check_matrix[:,:-1]) == np.linalg.matrix_rank(matrix) \
+           and np.linalg.matrix_rank(check_matrix[:,:-1]) < check_matrix[:,:-1].shape[0]:
+            raise ValueError('该线性方程组有多个解,暂时支持有唯一解的方程组')
+        
